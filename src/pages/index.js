74f5b22fd3e56affe5225b93d50189bb2477c6f9
Corey Hammond <coreyjhammond@gmail.com>
2020-01-14 12:52:18 -0800

    created index template and IndexQuery

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,33 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
+    <h2>Index</h2>
+    {data.allMarkdownRemark.edges.map(post => (
+      <li key={post.node.id}><Link href={post.node.frontmatter.path}>{post.node.frontmatter.title}</Link></li> 
+    ))}
   </Layout>
 )
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(limit: 10) {
+      edges {
+        node {
+          frontmatter {
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
